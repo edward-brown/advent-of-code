71a095f6e8ffef79e35baf63ef50555d3832323f
edward-brown <ed.brown@dotgolfinternational.com>
2024-12-02 13:09:03 +0000

    Day 1 2024, forgotten how to f#

---
+++ 2024/1/main.fsx
@@ -0,0 +1,18 @@
+let input = System.IO.File.ReadAllLines("2024/1/input.txt")
+
+let part1 () =
+    input
+    |> Seq.map (fun x -> (x.Split ' ') |> (fun a -> int a.[0], int a.[3])) 
+    |> Seq.toList
+    |> List.unzip
+    |> (fun (x, y) -> List.zip (List.sort x) (List.sort y))
+    |> Seq.sumBy (fun (x, y) -> (x - y) |> abs)
+
+let part2 () =
+    input 
+    |> Seq.map (fun x -> (x.Split ' ') |> (fun a -> int a.[0], int a.[3])) 
+    |> Seq.toList
+    |> List.unzip
+    |> (fun (x, y) -> x |> List.sumBy(fun a -> a * (y |> List.filter(fun b -> a = b) |> List.length)))
+
+part2 ()
